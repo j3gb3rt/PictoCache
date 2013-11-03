20b1a7ca61715cc504d459c1690135de9792dd32
Kyle McCaffrey <gtg310x@gatech.edu>
2013-11-03 00:54:19 -0400

    Reworked Camera Manager

--- ImagePail/src/main/java/edu/gatech/cs4261/wheresdabeef/camera/CameraManager.java
+++ ImagePail/src/main/java/edu/gatech/cs4261/wheresdabeef/camera/CameraManager.java
@@ -1,60 +1,114 @@
 package edu.gatech.cs4261.wheresdabeef.camera;
 
+import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.hardware.Camera;
 import android.hardware.Camera.ShutterCallback;
 import android.hardware.Camera.PictureCallback;
 import android.location.Location;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
+
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import edu.gatech.cs4261.wheresdabeef.domain.Image;
 import edu.gatech.cs4261.wheresdabeef.location.LocationApi;
 
-public class CameraManager {
+public class CameraManager extends Activity {
 
-    Bitmap image;
-    Location location;
+    private static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 100;
+    public static final int MEDIA_TYPE_IMAGE = 1;
+    public static final int MEDIA_TYPE_VIDEO = 2;
 
-	public class CMPicCallback implements PictureCallback{
+    private Bitmap image;
+    private Location location;
+    private Uri fileUri;
 
-		@Override
-		public void onPictureTaken(byte[] data, Camera camera) {
-            image = BitmapFactory.decodeByteArray(data , 0, data .length);
-        }
-		
-	}
-	
-	public class CMShutterCallback implements ShutterCallback{
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.main);
 
-		@Override
-		public void onShutter() {
-			location = LocationApi.stopPollingLocation();
-		}
-		
-	}
-	
-	private Camera camera;
+        // create Intent to take a picture and return control to the calling application
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
 
-    public CameraManager(){
+        fileUri = getOutputMediaFileUri(MEDIA_TYPE_IMAGE); // create a file to save the image
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri); // set the image file name
 
+        // start the image capture Intent
+        startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+
+        LocationApi.startPollingLocation(this);
     }
 
-	public void openCamera(Context context){
-        LocationApi.startPollingLocation(context);
-		camera = Camera.open();
-        try{
-            camera.startPreview();
-        }catch(NullPointerException npe){
-            // do nothing here
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                image = BitmapFactory.decodeFile(data.getData().getPath());
+                location = LocationApi.stopPollingLocation();
+            } else if (resultCode == RESULT_CANCELED) {
+                image = null;
+                location = null;
+            } else {
+                image = null;
+                location = null;
+            }
         }
-	}
-	
-	public Image takePicture(){
-		camera.takePicture(new CMShutterCallback(), null, null, new CMPicCallback());
-        camera.release();
-        Image im = new Image(image,location);
-        return im;
-	}
+    }
 
+    public Bitmap getImage() {
+        return this.image;
+    }
+
+    public Location getLocation() {
+        return this.location;
+    }
+
+    /** Create a file Uri for saving an image or video */
+    private static Uri getOutputMediaFileUri(int type){
+        return Uri.fromFile(getOutputMediaFile(type));
+    }
+
+    /** Create a File for saving an image or video */
+    private static File getOutputMediaFile(int type){
+        // To be safe, you should check that the SDCard is mounted
+        // using Environment.getExternalStorageState() before doing this.
+
+        File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES), "MyCameraApp");
+        // This location works best if you want the created images to be shared
+        // between applications and persist after your app has been uninstalled.
+
+        // Create the storage directory if it does not exist
+        if (! mediaStorageDir.exists()){
+            if (! mediaStorageDir.mkdirs()){
+                Log.d("MyCameraApp", "failed to create directory");
+                return null;
+            }
+        }
+
+        // Create a media file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        File mediaFile;
+        if (type == MEDIA_TYPE_IMAGE){
+            mediaFile = new File(mediaStorageDir.getPath() + File.separator +
+                    "IMG_"+ timeStamp + ".jpg");
+        } else if(type == MEDIA_TYPE_VIDEO) {
+            mediaFile = new File(mediaStorageDir.getPath() + File.separator +
+                    "VID_"+ timeStamp + ".mp4");
+        } else {
+            return null;
+        }
+
+        return mediaFile;
+    }
 }
